a3fb6b3bd7e4303c7807899b756b3cff42741b2b
Your Name <you@example.com>
2022-03-15 11:50:50 +0530

    first method added

--- myApp.js
+++ myApp.js
@@ -2,10 +2,9 @@ var express = require("express");
 const res = require("express/lib/response");
 var app = express();
 
-app.get("/", function (req, resp) {
-  res.send("Response String");
+app.get("/", function (req, res) {
+  res.send("Hello Express");
 });
-
 console.log("Hello World");
 
 module.exports = app;

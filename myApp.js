ade1fa8115c8cb4f2edddc781e16473859047d21
Your Name <you@example.com>
2022-03-15 12:13:01 +0530

    static path added

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@ var express = require("express");
 const res = require("express/lib/response");
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
   absolutePath = __dirname + "/views/index.html";
 
